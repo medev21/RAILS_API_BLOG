7135865de4ea5eb594ceffd7cc448175e424d017
Martin Ben <mebenav88@gmail.com>
2016-04-08 19:18:50 -0500

    add splice to remove post and update scope.posts; add comments

--- app/assets/javascripts/angular-app/controllers/main.js
+++ app/assets/javascripts/angular-app/controllers/main.js
@@ -3,22 +3,25 @@ angular.module('app')
        .controller('MainCtrl',['Post', '$scope', function(Post, $scope){
 
 
-        $scope.posts = Post.query();
-        $scope.newPost = new Post();
+        $scope.posts = Post.query(); //get all posts
+        $scope.newPost = new Post(); //empty newPost
 
         //adding a post
         $scope.addPost = function(post){
 
-          post.$save();
-          $scope.posts.push(post);
+          post.$save(); //save post to rails
+          $scope.posts.push(post); //pushes new post to the $scope.posts - query
           console.log($scope.posts);
-          $scope.newPost = new Post();
-          $scope.posts = Post.query();
+          $scope.newPost = new Post(); //set newPost empty
+          $scope.posts = Post.query(); //get all posts and update it
         }
 
         //destroying a post
-        $scope.removePost = function(post){
-          Post.delete(post);
+        // function index same as $index
+        $scope.removePost = function(index){
+          post = $scope.posts[index]; //find post w/ index from $scope.posts and set that into a var
+          Post.delete(post); //delete that post
+          $scope.posts.splice(index, 1); //update $scope.posts by removing deleted post
         }
 
       }]);
